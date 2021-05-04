c7284f974f1e19e498de3dc4505cd15ce29e86c4
Daniil_Gayvoronsky <qwer.82@mail.ru>
2021-05-04 13:30:31 +0300

    Add DZ1

---
+++ run.py
@@ -0,0 +1,9 @@
+from daniel_framework.main import Framework
+from urls import routes, fronts
+from wsgiref.simple_server import make_server
+
+application = Framework(routes, fronts)
+
+with make_server("127.0.0.5", 8080, application) as httpd:
+    print("Запуск на порту 8080...")
+    httpd.serve_forever()
