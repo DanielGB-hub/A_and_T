d90b9aec267b9c3f724c1dea0e15b033927cc720
Daniil_Gayvoronsky <qwer.82@mail.ru>
2021-05-06 17:46:38 +0300

    Add DZ3

---
+++ urls.py
@@ -0,0 +1,24 @@
+from datetime import date
+from views import Index, About, Contacts
+
+
+# front controller
+# Переводим дату в месяц (для этого варианта)
+def secret_front(request):
+    month_dict = {1: "Январь", 2: "Февраль", 3: "Март", 4: "Апрель", 5: "Май", 6: "Июнь", 7: "Июль", 8: "Август",
+                  9: "Сентябрь", 10: "Октябрь", 11: "Ноябрь", 12: "Декабрь"}
+    i = date.today().month
+    request['data'] = month_dict[i]
+
+
+def other_front(request):
+    request['key'] = 'key'
+
+
+fronts = [secret_front, other_front]
+
+routes = {
+    '/': Index(),
+    '/about/': About(),
+    '/contacts/': Contacts(),
+}
